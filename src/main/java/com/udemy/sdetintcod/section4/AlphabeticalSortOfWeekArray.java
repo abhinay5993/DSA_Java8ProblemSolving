fa7859a90e39df5e84acffd8715e60e372a19a87
abhinay5993 <abhinaylunawat5993@gmail.com>
2026-02-26 23:59:53 +0530

    Added all latest problem solutions for section4

--- src/main/java/com/udemy/sdetintcod/section4/AlphabeticalSortOfWeekArray.java
+++ src/main/java/com/udemy/sdetintcod/section4/AlphabeticalSortOfWeekArray.java
@@ -14,8 +14,14 @@ public class AlphabeticalSortOfWeekArray {
 	String[] strWeekArray={"sun","mon","tue","wed","thu","fri","sat"};
 	System.out.println("\nInput string array : "+Arrays.toString(strWeekArray));
 	System.out.println("\nResultant Sorted Array : "+Arrays.toString(alphaSortedArrayItems(strWeekArray)));
+	System.out.println("*****************************************************************");
+	
+	String[] studNames= {"Atif","Chuhan","KK","Aslam","Farhan","Zaved","Bilal","Faisal","Noor","Lata","Mika","Sonu"};
+	performBubbleSortOnTextArray(studNames);
+	
 	}
 
+	
 	/**
 	 * Using TreeSet inbuilt sorted property
 	 * 
@@ -29,5 +35,32 @@ public class AlphabeticalSortOfWeekArray {
 		}
 		return sortedSet.toArray(new String[0]);
 	}
+	
+	
+	/**
+	 * Do bubble sort on Text array
+	 * 
+	 * @param strNamesArr
+	 * 
+	 */
+	private static void performBubbleSortOnTextArray(String[] strNamesArr) {
+		int arrayLen = strNamesArr.length - 1;
+
+		for (int i = 0; i < arrayLen; i++) {
+			for (int j = 0; j < arrayLen - i; j++) {
+				if (strNamesArr[j].compareTo(strNamesArr[j + 1]) > 0) {
+					String strTemp = strNamesArr[j];
+					strNamesArr[j] = strNamesArr[j + 1];
+					strNamesArr[j + 1] = strTemp;
+				}
+			}
+		}
+
+		System.out.println("\n********* Sorted Names *********************");
+		for (String strNameItems : strNamesArr) {
+			System.out.print(strNameItems + ", ");
+		}
+	}
+	
 
 }
